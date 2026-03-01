8bafe67c92c9bd3917eaf1e64d0313a858590372
Nikolay Boshnakov <n_boshnakov@abv.bg>
2026-03-01 16:30:58 +0200

    Push final engineering tasks

--- Week_01/tasks/engineering/task07.py
+++ Week_01/tasks/engineering/task07.py
@@ -2,16 +2,19 @@ try:
     from . import task01, task06, task03, task05
 except ImportError:
     import task01, task06, task03, task05
+import numpy as np
 
+rng2 = np.random.default_rng(123)
 
 def climb_fall_chance(curr_step):
-    fall_chance = task01.rng.random()
+    fall_chance = rng2.random()
     if fall_chance <= 0.005:
-        return 0
+        return task03.empire_state_climb_fixed(0)
     else:
         return task03.empire_state_climb_fixed(curr_step)
 
 
 if __name__ == '__main__':
     walks_list = task05.multiple_walks(100, 20, climb_fall_chance)
+    print(task05.multiple_walks(100, 1, climb_fall_chance))
     task06.steps_line_plot_multiple(walks_list)
