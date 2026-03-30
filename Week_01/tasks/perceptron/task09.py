a447a7a5ad471d05bd28499effb31cd3d4343fe0
Nikolay Boshnakov <n_boshnakov@abv.bg>
2026-03-30 22:31:02 +0300

    Work on updating the code for tasks 9-10

--- Week_01/tasks/perceptron/task09.py
+++ Week_01/tasks/perceptron/task09.py
@@ -1,117 +1,32 @@
-import matplotlib.pyplot as plt
+import numpy as np
 try:
-    from . import task01, task06, task07, task08
+    from . import task01, task08
 except ImportError:
-    import task01, task06, task07, task08
+    import task01, task08
 
 
-class ModelNAND():
-    def __init__(self, w1=task01.initialize_weights(0, 10), w2=task01.initialize_weights(0, 10), w3=task01.initialize_weights(0, 10)):
-        self.w1 = w1
-        self.w2 = w2
-        self.w3 = w3
-        epochs = 100000
-        learning_rate = 0.001
-
-        dataset_AND = [(0, 0, 0), (0, 1, 0), (1, 0, 0), (1, 1, 1)]
-        dataset_NOT = [(0, 1), (1, 0)]
-        self.perceptron_AND = task08.PerceptronSigmoid(dataset_AND, self.w1, self.w2)
-        self.perceptron_AND.train(epochs, learning_rate)
-        self.perceptron_NOT = PerceptronSigmoidSingle(dataset_NOT, self.w3)
-        self.perceptron_NOT.train(epochs, learning_rate)
-
-    def predict(self, input1, input2):
-
-        pred_AND = (1 if self.perceptron_AND.predict(input1, input2) >= 0.5 else 0)
-
-        return (self.perceptron_NOT.predict(pred_AND))
-
-class PerceptronSigmoidSingle():
-    def __init__(self, dataset, w1=task01.initialize_weights(0, 10)):
-        self.w1 = w1
-        self.dataset = dataset
-        self.bias = task01.initialize_weights(0, 1)
-        self.loss_list = []
-
-    def calc_loss(self, w1_eps: float=0, b_eps: float=0):
-        sum = 0
-        for (x, z) in self.dataset:
-            sum += (task07.sigmoid((x * (self.w1 + w1_eps)) + (self.bias + b_eps)) - z)**2
-    
-        return sum / len(self.dataset)
-    
-    def calc_derivative(self, w1_eps: float=0, b_eps: float=0):
-        loss1 = self.calc_loss()
-        loss2 = self.calc_loss(w1_eps, b_eps)
-        self.loss_list.append((loss2 - loss1) / (w1_eps if w1_eps != 0 else b_eps))
-
-        return (loss2 - loss1) / (w1_eps if w1_eps != 0 else b_eps)
-    
-    def single_step(self, learning_rate):
-        print(f"Loss before: {self.calc_loss()}")
-        d_w1 = self.calc_derivative(w1_eps = 0.001)
-        d_b = self.calc_derivative(b_eps = 0.001)
-
-        self.w1 -= d_w1 * learning_rate
-        self.bias -= d_b * learning_rate
-        print(f"Loss after: {self.calc_loss()}")
-        return self.w1
-
-    def train(self, epochs, learning_rate=0.001):
-        for n in range(epochs):
-            self.single_step(learning_rate)
-            pass
-
-    def predict(self, input1):
-        return (task07.sigmoid((self.w1*input1) + self.bias))
 
 def main():
-    modelNAND = ModelNAND()
+    NAND_dataset = [(0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 0)]
+    epochs = 100000
+    learning_rate = 0.01
 
-    # testing NOT perceptron
-    print(f"NOT for 1: {modelNAND.perceptron_NOT.predict(1)}")
-    print(f"NOT for 0: {modelNAND.perceptron_NOT.predict(0)}")
+    # This should have worked, no idea why it didn't. I tried to solve the task by building a multi-layer perceptron (https://elcaiseri.medium.com/building-a-multi-layer-perceptron-from-scratch-with-numpy-e4cee82ab06d) but hit a wall with the implementation
+    perceptron_NAND = task08.PerceptronSigmoid(NAND_dataset, task01.initialize_weights(-1, 1), task01.initialize_weights(-1, 1))
 
-    # NAND Model results
-    print(f"NAND for 1 and 1: {modelNAND.predict(1, 1)}")
-    print(f"NAND for 0 and 1: {modelNAND.predict(0, 1)}")
-    print(f"NAND for 1 and 0: {modelNAND.predict(1, 0)}")
-    print(f"NAND for 0 and 0: {modelNAND.predict(0, 0)}")
-    
+    perceptron_NAND.train(epochs, learning_rate)
+
+    # tests
+    print(f"NAND for 1 and 1: {perceptron_NAND.predict(1, 1)}")
+    print(f"NAND for 0 and 1: {perceptron_NAND.predict(0, 1)}")
+    print(f"NAND for 1 and 0: {perceptron_NAND.predict(1, 0)}")
+    print(f"NAND for 0 and 0: {perceptron_NAND.predict(0, 0)}")
+
+    # Returns:
+    # NAND for 0 and 1: 0.9177018823301617
+    # NAND for 1 and 0: 0.9177018983853135
+    # NAND for 0 and 0: 0.9991251477290325
+    # NAND for 1 and 1: 0.09818717680303782
+
 if __name__ == '__main__':
     main()
-
-
-# Previous solution, reusing the perceptron class created for AND and OR; Did not work
-# import numpy as np
-# try:
-#     from . import task01, task06, task07, task08
-# except ImportError:
-#     import task01, task06, task07, task08
-
-
-
-# def main():
-#     NAND_dataset = [(0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 0)]
-#     epochs = 100000
-#     learning_rate = 0.001
-
-#     # This should have worked, no idea why it didn't. I tried to solve the task by building a multi-layer perceptron (https://elcaiseri.medium.com/building-a-multi-layer-perceptron-from-scratch-with-numpy-e4cee82ab06d) but hit a wall with the implementation
-#     perceptron_NAND = task08.PerceptronSigmoid(NAND_dataset)
-
-#     perceptron_NAND.train(epochs, learning_rate)
-
-#     # tests
-#     print(f"NAND for 1 and 1: {perceptron_NAND.predict(1, 1)}")
-#     print(f"NAND for 0 and 1: {perceptron_NAND.predict(0, 1)}")
-#     print(f"NAND for 1 and 0: {perceptron_NAND.predict(1, 0)}")
-#     print(f"NAND for 0 and 0: {perceptron_NAND.predict(0, 0)}")
-
-#     # Returns:
-#     # NAND for 1 and 1: 0.999999994046725
-#     # NAND for 0 and 1: 0.999900929781145
-#     # NAND for 1 and 0: 0.999991780877754
-#     # NAND for 0 and 0: 0.8796687605047867
-
-# if __name__ == '__main__':
-#     main()
--- Week_01/tasks/perceptron/task10.py
+++ Week_01/tasks/perceptron/task10.py
@@ -6,53 +6,60 @@ except ImportError:
 
 
 class ModelXOR():
-    def __init__(self, w11=task01.initialize_weights(0, 10), w12=task01.initialize_weights(0, 10), w13=task01.initialize_weights(0, 10), w14=task01.initialize_weights(0, 10), w21=task01.initialize_weights(0, 10), w22=task01.initialize_weights(0, 10)):
-        # self.w11 = w11
-        # self.w12 = w12
-        # self.w13 = w13
-        # self.w14 = w14
-        # self.w21 = w21
-        # self.w22 = w22
-        epochs = 100000
-        learning_rate = 0.001
+    def __init__(self, dataset, w1_or=task01.initialize_weights(-1, 1), w2_or=task01.initialize_weights(-1, 1), w1_nand=task01.initialize_weights(-1, 1), w2_nand=task01.initialize_weights(-1, 1), w1_and=task01.initialize_weights(-1, 1), w2_and=task01.initialize_weights(-1, 1)):
+        self.w1_or = w1_or
+        self.w2_or = w2_or
+        self.w1_nand = w1_nand
+        self.w2_nand = w2_nand
+        self.w1_and = w1_and
+        self.w2_and = w2_and
+        self.b_or = task01.initialize_weights(-1, 1)
+        self.b_nand = task01.initialize_weights(-1, 1)
+        self.b_and = task01.initialize_weights(-1, 1)
+        self.dataset = dataset
 
-        dataset_AND = [(0, 0, 0), (0, 1, 0), (1, 0, 0), (1, 1, 1)]
-        dataset_OR = [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1)]
-        dataset_NAND = [(0, 0, 1), (0, 1, 1), (1, 0, 1), (1, 1, 0)]
-        self.perceptron_OR = task08.PerceptronSigmoid(dataset_OR)
-        # self.perceptron_NAND = task08.PerceptronSigmoid(dataset_NAND)
-        self.perceptron_NAND = task09.ModelNAND()
-        self.perceptron_OR.train(epochs, learning_rate)
-        # self.perceptron_NAND.train(epochs, learning_rate)
-        self.perceptron_AND = task08.PerceptronSigmoid(dataset_AND)
-        self.perceptron_AND.train(epochs, learning_rate)
-        print(f"Sigmoid - AND for 1 and 1: {self.perceptron_AND.predict(1, 1)}")
-        print(f"Sigmoid - AND for 0 and 1: {self.perceptron_AND.predict(0, 1)}")
-        print(f"Sigmoid - AND for 1 and 0: {self.perceptron_AND.predict(1, 0)}")
-        print(f"Sigmoid - AND for 0 and 0: {self.perceptron_AND.predict(0, 0)}")
-        print("------------------------------------------------------------")
-        print(f"Sigmoid - OR for 1 and 1: {self.perceptron_OR.predict(1, 1)}")
-        print(f"Sigmoid - OR for 0 and 1: {self.perceptron_OR.predict(0, 1)}")
-        print(f"Sigmoid - OR for 1 and 0: {self.perceptron_OR.predict(1, 0)}")
-        print(f"Sigmoid - OR for 0 and 0: {self.perceptron_OR.predict(0, 0)}")
-        print("------------------------------------------------------------")
-        print(f"NAND for 1 and 1: {self.perceptron_NAND.predict(1, 1)}")
-        print(f"NAND for 0 and 1: {self.perceptron_NAND.predict(0, 1)}")
-        print(f"NAND for 1 and 0: {self.perceptron_NAND.predict(1, 0)}")
-        print(f"NAND for 0 and 0: {self.perceptron_NAND.predict(0, 0)}")
+    def forward(self, data, w1_or_eps: float=0, w2_or_eps: float=0, w1_nand_eps: float=0, w2_nand_eps: float=0, w1_and_eps: float=0, w2_and_eps: float=0, b_or_eps: float=0, b_nand_eps: float=0, b_and_eps: float=0):
+        out_or = (data[0] * (self.w1_or + w1_or_eps) + data[1] * (self.w2_or + w2_or_eps)) + (self.b_or + b_or_eps)
+        out_nand = (data[0] * (self.w1_nand + w1_nand_eps) + data[1] * (self.w2_nand + w2_nand_eps)) + (self.b_nand + b_nand_eps)
+        out_and = (data[0] * (self.w1_and + w1_and_eps) + data[1] * (self.w2_and + w2_and_eps)) + (self.b_and + b_and_eps)
+        return out_and
 
+    def calc_loss(self, w1_or_eps: float=0, w2_or_eps: float=0, w1_nand_eps: float=0, w2_nand_eps: float=0, w1_and_eps: float=0, w2_and_eps: float=0, b_or_eps: float=0, b_nand_eps: float=0, b_and_eps: float=0):
+        sum = 0
+        for (x, y, z) in self.dataset:
+            sum += (task07.sigmoid((x * (self.w1 + w1_or_eps) + y * (self.w2 + w2_or_eps)) + (self.b_or + b_or_eps)) - z)**2
+    
+        return sum / len(self.dataset)
+    
+    def calc_derivative(self, w1_eps: float=0, w2_eps: float=0, b_eps: float=0):
+        loss1 = self.calc_loss()
+        loss2 = self.calc_loss(w1_eps, w2_eps, b_eps)
+        self.loss_list.append((loss2 - loss1) / (w1_eps if w1_eps != 0 else w2_eps if w2_eps != 0 else b_eps))
+
+        return (loss2 - loss1) / (w1_eps if w1_eps != 0 else w2_eps if w2_eps != 0 else b_eps)
+    
+    def single_step(self, learning_rate):
+        print(f"Loss before: {self.calc_loss()}")
+        d_w1 = self.calc_derivative(w1_eps = 0.001)
+        d_w2 = self.calc_derivative(w2_eps = 0.001)
+        d_b = self.calc_derivative(b_eps = 0.001)
+
+        self.w1 -= d_w1 * learning_rate
+        self.w2 -= d_w2 * learning_rate
+        self.bias -= d_b * learning_rate
+        print(f"Loss after: {self.calc_loss()}")
+        return self.w1, self.w2
+
+    def train(self, epochs, learning_rate=0.001):
+        for n in range(epochs):
+            self.single_step(learning_rate)
+            pass
 
     def predict(self, input1, input2):
-        pred_OR = (1 if self.perceptron_OR.predict(input1, input2) >= 0.5 else 0)
-        print(f"Pred_OR: {pred_OR}")
-        pred_NAND = (1 if self.perceptron_NAND.predict(input1, input2) >= 0.5 else 0)
-        print(f"Pred_NAND: {pred_NAND}")
-
-        return (self.perceptron_AND.predict(pred_OR, pred_NAND))
-
-
+        return (task07.sigmoid((self.w1*input1 + self.w2*input2) + self.bias))
 def main():
-    modelXOR = ModelXOR()
+    dataset_XOR = [(0, 0, 1), (0, 1, 0), (1, 0, 0), (1, 1, 1)]
+    modelXOR = ModelXOR(dataset_XOR)
 
     # bias
     print(f"XOR for 1 and 1: {modelXOR.predict(1, 1)}")
